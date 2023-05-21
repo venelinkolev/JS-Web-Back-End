a24cb0456fd9aa7cc762e0bd3fae5e609cfe5674
venelinkolev <114242353+venelinkolev@users.noreply.github.com>
2023-05-21 20:31:24 +0300

    Cats Express

--- 02-Cat-Shelter-Workshop-Express/controllers/homeController.js
+++ 02-Cat-Shelter-Workshop-Express/controllers/homeController.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
+const fs = require('fs').promises;
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const cats = JSON.parse(await fs.readFile('./data/cats.json', 'utf8'));
+
   res.render('home', {
     title: 'Cat Shelter',
     isHome: true,
+    cats,
   });
 });
 
